a08deba9692820646880452167aeda15fcf4b084
bastian <bastiangarcesga@gmail.com>
2023-04-26 11:21:37 -0400

    as

--- src/auth/google.service.ts
+++ src/auth/google.service.ts
@@ -27,10 +27,8 @@ export class GoogleAuthService {
   generateGoogleAuthUrl(role: string): string {
     const scope = ['profile', 'email'];
     const redirectUrl = process.env.GOOGLE_REDIRECT_URL;
-    const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${
-      process.env.GOOGLE_CLIENT_ID
-    }&redirect_uri=${redirectUrl}&response_type=code&scope=${scope.join('%20')}&state=${role}`;
-    console.log(url);
+    const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${redirectUrl}&response_type=code&scope=${scope}&state=${role}`;
+    console.log('url', url);
     return url;
   }
 
